454a8abbc2bf1ea4d636af3cef42093ed5ac1eef
PartyProNL <partypronl@gmail.com>
2021-06-07 15:39:47 +0200

    Added some more functionality, like getting the location and summary of the weatherreport

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,18 @@
 const init = async () => {
     let data = await getData();
-    getLocation();
+    navigator.geolocation.getCurrentPosition(getLocation);
 
     console.log(data);
+    console.log(data.forecast.weatherreport.summary);
+
+    navigator.geolocation.getCurrentPosition(showPosition);
+
+    setTimeout(function() {
+        console.log(cityName)
+    }, 3000)
 }
 
-var currentLat;
-var currentLon;
+var cityName = null;
 
 async function getData() {
     let temp = await fetch("https://data.buienradar.nl/2.0/feed/json")
@@ -19,7 +25,7 @@ async function getData() {
     return temp;
 }
 
-function getLocation() {
+function getLocation(position) {
     if(localStorage.currentLatS && localStorage.currentLonLS) {
         currentLat = position.coords.latitude;
         currentLon = position.coords.longitude;
@@ -31,6 +37,35 @@ function getLocation() {
             alert('Huidige locatie opvragen wordt helaas niet ondersteund!')
         }
     }
+}
+
+// UTILITIES
+async function getCurrentLocationData(lat, lon) {
+    let temp = await fetch("https://nominatim.openstreetmap.org/reverse?format=json&lat=" + lat + "&lon=" + lon)
+    .then((x) => {
+        return x.json()
+    })
+    .catch((error) => {
+        console.error(error)
+    })
+    return temp;
+}
+
+function getCurrentLocation(stuff) {
+    if(stuff.address.city) {
+        return stuff.address.city;
+    } else if(stuff.address.village) {
+        return stuff.address.village;
+    }
+}
+
+async function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude + 
+    " Longitude: " + position.coords.longitude);
+
+    let locData = await getCurrentLocationData(position.coords.latitude, position.coords.longitude);
+    //console.log(getCurrentLocation(locData))
+    cityName = getCurrentLocation(locData);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
