e4917f9690c457aac9897bebf323f429d17bd900
PartyProNL <partypronl@gmail.com>
2021-06-18 17:11:26 +0200

    Finished

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,8 @@
 const init = async () => {
     data = await getData();
-    navigator.geolocation.getCurrentPosition(getLocation);
-
-    console.log(data);
-    console.log(data.forecast.weatherreport.summary);
-    console.log(getNearestWeatherStation())
-
-    navigator.geolocation.getCurrentPosition(showPosition);
-
-    setTimeout(function() {
-        console.log(cityName)
-    }, 3000)
-
-    var userWeatherStationId = 0
+    navigator.geolocation.getCurrentPosition(saveCurrentPosition);
+    console.log(getNearestWeatherStation("temperature"))
+    var userWeatherStationId = getIndex(getNearestWeatherStation("temperature"))
 
     document.querySelector(".pretparkVoorspelling").innerHTML = data.actual.stationmeasurements[userWeatherStationId].weatherdescription
     document.querySelector(".iconWind").innerHTML = data.actual.stationmeasurements[userWeatherStationId].windspeed + " km/h wind"
@@ -78,6 +68,21 @@ async function getCurrentLocationData(lat, lon) {
     return temp;
 }
 
+function saveCurrentPosition(position) {
+    // Is er een position meegegeven?
+    if(position !== null) {
+        // Opslaan in localstorage
+        currentLat = position.coords.latitude;
+        currentLon = position.coords.longitude;
+        localStorage.currentLatLS = currentLat;
+        localStorage.currentLonLS = currentLon;
+    } else {
+        // Geen positie meegegeven, ophalen uit local storage
+        currentLat = localStorage.currentLatLS;
+        currentLon = localStorage.currentLonLS;
+    }
+}
+
 function getCurrentLocation(stuff) {
     if(stuff.address.city) {
         return stuff.address.city;
@@ -94,24 +99,44 @@ async function showPosition(position) {
     cityName = getCurrentLocation(locData);
 }
 
-function getNearestWeatherStation() {
+function getNearestWeatherStation(searchValue) {
+
+    // All weerstations
     weerstations = data.actual.stationmeasurements;
 
+    // Variabelen om mee te werken
     var dichtbij;
     var afstand = 1000;
 
+    // Ieder weerstation aflopen
     for (var weerstation in weerstations) {
         if (weerstations.hasOwnProperty(weerstation)) {
-            tempAfstand = getDistanceFromLatLonInKm(weerstations[weerstation].lat, weerstations[weerstation].lon, currentLat, currentLon);
 
-            if(afstand > tempAfstand) {
-                afstand = tempAfstand;
-                dichtbij = weerstations[weerstation];
+            // Afstand bepalen ten opzichte van eigen locatie en locatie weerstation
+            tempAfstand = getDistanceFromLatLonInKm(weerstations[weerstation].lat, weerstations[weerstation].lon, currentLat, currentLon);
+            //console.log(weerstations[weerstation].stationname + ": " + tempAfstand + "km");
+
+            // Is deze afstand korter dan een eerder gevonden weerstation?
+            if(afstand < tempAfstand) {
+                // EN bevat dit weerstation de gevraagde informatie?
+                if(weerstations[weerstation].hasOwnProperty(searchValue) !== undefined) {
+                    // Dan is dit nu de dichstbijzijnde
+                    afstand = tempAfstand;
+                    dichtbij = weerstations[weerstation];
+                }
             }
         }
     }
 
     return dichtbij;
+}
+
+function getIndex(weatherStation) {
+    for (var i = 0; i < data.actual.stationmeasurements.length; i++) {
+        if(data.actual.stationmeasurements[i].stationname == weatherStation.stationname) {
+            return i
+        }
+    }
 }
 
 function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
